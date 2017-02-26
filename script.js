678414d28b54aa289b1a8b7b8367e4faea3daddc
Carlo Panzi <carlo@uberresearch.com>
2017-02-26 18:39:34 +0100

    scroll

---
+++ script.js
@@ -0,0 +1,31 @@
+(function() {
+	var arrow = document.getElementById('arrow');
+	var secondBlock = document.getElementById('secondblock');
+
+	var scroll = function (elementY, duration) { 
+		var startingY = window.pageYOffset;
+		var diff = elementY - startingY;
+		var start;
+
+		window.requestAnimationFrame(function step(timestamp) {
+			if (!start) {
+				start = timestamp;
+			}
+			var time = timestamp - start;
+			var percent = Math.min(time / duration, 1);
+
+			window.scrollTo(0, startingY + diff * percent);
+
+			if (time < duration) {
+				window.requestAnimationFrame(step);
+			}
+		})
+	}
+	
+	arrow.addEventListener('click', function() {
+		scroll(secondBlock.offsetTop, 300);
+	})
+
+})();
+
+
